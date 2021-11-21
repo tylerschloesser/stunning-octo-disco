1787ea853d9cdd59951e3c5d21b1c9eca031e145
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-21 10:37:27 -0800

    add target

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,4 @@
-import { random, times } from 'lodash'
+import { isEqual, random, times } from 'lodash'
 import { Pointer } from './input'
 
 class Vec2 {
@@ -21,7 +21,7 @@ class Vec2 {
 interface Thing {
   p: Vec2
   v: Vec2
-  target?: Vec2
+  target: Vec2 | null
 }
 
 export interface State {
@@ -34,6 +34,7 @@ export function init(): State {
     return {
       p: new Vec2(random(100), random(100)),
       v: new Vec2(random(1), random(1)),
+      target: null,
     }
   })
 
@@ -41,10 +42,21 @@ export function init(): State {
 }
 
 export function tick(state: State, pointer: Pointer | null, dt: number): State {
+  const updateTarget = isEqual(state.pointer, pointer)
   const things = state.things.map((thing) => {
+    let v = thing.v
+    let target = thing.target
+    if (updateTarget) {
+      if (pointer) {
+        target = new Vec2(pointer.x, pointer.y)
+      } else {
+        target = null
+      }
+    }
     return {
       ...thing,
-      p: thing.p.add(thing.v.multiply(dt / 1000)),
+      p: thing.p.add(v.multiply(dt / 1000)),
+      target,
     }
   })
   return {
