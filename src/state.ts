ef36cd79897c7a0121236a0dafbf96d8d799ffdb
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-21 10:29:46 -0800

    add state

---
+++ src/state.ts
@@ -0,0 +1,49 @@
+import { random, times } from 'lodash'
+
+class Vec2 {
+  readonly x: number
+  readonly y: number
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  multiply(scalar: number) {
+    return new Vec2(this.x * scalar, this.y * scalar)
+  }
+
+  add(v: Vec2) {
+    return new Vec2(this.x + v.x, this.y + v.y)
+  }
+}
+
+interface Thing {
+  p: Vec2
+  v: Vec2
+}
+
+export interface State {
+  things: Thing[]
+}
+
+export function init(): State {
+  const things: Thing[] = times(10, () => {
+    return {
+      p: new Vec2(random(100), random(100)),
+      v: new Vec2(random(1), random(1)),
+    }
+  })
+
+  return { things }
+}
+
+export function tick(state: State, dt: number): State {
+  const things = state.things.map((thing) => ({
+    ...thing,
+    p: thing.p.add(thing.v.multiply(dt / 1000)),
+  }))
+  return {
+    ...state,
+    things,
+  }
+}
