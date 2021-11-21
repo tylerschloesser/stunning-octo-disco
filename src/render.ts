2e3664971692c12d742c783a12fc36029412fc8d
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-21 09:53:03 -0800

    fill grey

--- src/render.ts
+++ src/render.ts
@@ -2,5 +2,9 @@ export function render(
   context: CanvasRenderingContext2D,
   canvas: HTMLCanvasElement,
 ) {
-  context.clearRect(0, 0, canvas.width, canvas.height)
+  const w = canvas.width
+  const h = canvas.height
+  context.clearRect(0, 0, w, h)
+  context.fillStyle = '#333'
+  context.fillRect(0, 0, w, h)
 }
