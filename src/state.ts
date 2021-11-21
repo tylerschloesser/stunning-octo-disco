e8f9ec21691b5c5c9db78bb853c5661375b3f972
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-21 10:42:22 -0800

    refactor vec2

--- src/state.ts
+++ src/state.ts
@@ -1,31 +1,6 @@
 import { isEqual, random, times } from 'lodash'
 import { Pointer } from './input'
-
-class Vec2 {
-  readonly x: number
-  readonly y: number
-  constructor(x: number, y: number) {
-    this.x = x
-    this.y = y
-  }
-
-  multiply(scalar: number) {
-    return new Vec2(this.x * scalar, this.y * scalar)
-  }
-
-  add(v: Vec2) {
-    return new Vec2(this.x + v.x, this.y + v.y)
-  }
-
-  subtract(v: Vec2) {
-    return new Vec2(this.x - v.y, this.y - v.y)
-  }
-
-  normalize() {
-    const dist = Math.sqrt(this.x * this.x - this.y * this.y)
-    return new Vec2(this.x / dist, this.y / dist)
-  }
-}
+import { Vec2 } from './vec2'
 
 interface Thing {
   p: Vec2
---
+++ src/vec2.ts
@@ -0,0 +1,25 @@
+export class Vec2 {
+  readonly x: number
+  readonly y: number
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  multiply(scalar: number) {
+    return new Vec2(this.x * scalar, this.y * scalar)
+  }
+
+  add(v: Vec2) {
+    return new Vec2(this.x + v.x, this.y + v.y)
+  }
+
+  subtract(v: Vec2) {
+    return new Vec2(this.x - v.y, this.y - v.y)
+  }
+
+  normalize() {
+    const dist = Math.sqrt(this.x * this.x - this.y * this.y)
+    return new Vec2(this.x / dist, this.y / dist)
+  }
+}
