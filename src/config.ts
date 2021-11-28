d6c5e3d448aeb8106e2f6a916a2655414e478c53
Tyler Schloesser <tylerschloesser@gmail.com>
2021-11-28 07:40:11 -0800

    save config

--- src/config.ts
+++ src/config.ts
@@ -6,13 +6,31 @@ let config: Config = {
   debug: false,
 }
 
+try {
+  const configJson = localStorage.getItem('config')
+  if (configJson) {
+    config = JSON.parse(configJson)
+  }
+} catch (e) {
+  console.log('failed to read config', e)
+}
+
 export function getConfig(): Config {
   return config
 }
 
+function setConfig(next: Config) {
+  try {
+    localStorage.setItem('config', JSON.stringify(next))
+  } catch (e) {
+    console.log('failed to save config', e)
+  }
+  config = next
+}
+
 export function toggleDebug() {
-  config = {
+  setConfig({
     ...config,
     debug: !config.debug,
-  }
+  })
 }
